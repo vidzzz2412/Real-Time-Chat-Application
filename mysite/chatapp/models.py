a648b62f1858ccffc6969ab17342a90cabcf1c5f
vidzzz24 <129959170+vidzzz24@users.noreply.github.com>
2024-08-02 23:05:46 +0530

    Flies upload

---
+++ mysite/chatapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+def __str__(self):
+    return self.name
+
+class ChatMessage(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    room = models.ForeignKey(ChatRoom , on_delete=models.CASCADE)
+    message_content = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=('date',)
+
+
+
